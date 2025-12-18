aabf369c38f85ee979418bd87f26d3deed87d808
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2025-12-18 14:21:18 +0100

    added test for bin K, test for macro K started

--- tests/test_error.py
+++ tests/test_error.py
@@ -1,95 +1,60 @@
 import numpy as np
 import pytest
 
-from quacc import error
-from quacc.legacy.data import ExtendedPrev, ExtensionPolicy
+from cap.error import K_bin, K_macro
+
+
+def simulate_ct(ct):
+    y_count = np.around(ct.sum(axis=1) * 1000, decimals=0)
+    y_hat_count = np.around(ct.sum(axis=0) * 1000, decimals=0)
+
+
+@pytest.fixture
+def k_macro_fix():
+    eps = 1e-3
+    for i in range(10):
+        ct = np.random.random(3**2)
+        ct += eps
+        ct /= ct.sum()
+        ct.reshape(3, 3)
 
 
 @pytest.mark.err
 class TestError:
     @pytest.mark.parametrize(
-        "prev,result",
+        "ct,result",
         [
-            (np.array([[1, 4], [4, 4]]), 0.5),
-            (np.array([[6, 2, 4], [2, 4, 2], [4, 2, 6]]), 0.5),
+            (np.array([[0.1, 0.3], [0.2, 0.4]]), -0.08333333333333337),
+            (np.array([[0.0, 0.0], [0.4, 0.6]]), 0.2),
+            (np.array([[0.4, 0.6], [0.0, 0.0]]), -0.2),
         ],
     )
-    def test_f1(self, prev, result):
-        ep = ExtendedPrev(prev.flatten(), prev.shape[0], extpol=ExtensionPolicy())
-        assert error.f1(prev) == result
-        assert error.f1(ep) == result
+    def test_k_bin(self, ct, result):
+        assert np.isclose(K_bin(ct), result)
 
     @pytest.mark.parametrize(
-        "prev,result",
+        "ct,result",
         [
-            (np.array([[4, 4], [4, 4]]), 0.5),
-            (np.array([[2, 4, 2], [2, 2, 4], [4, 2, 2]]), 0.25),
+            (np.array([[0.2, 0.07, 0.25], [0.08, 0.1, 0.03], [0.07, 0.15, 0.05]]), 0.02381612),
+            # (np.array([[0.0, 0.0], [0.4, 0.6]]), 0.2),
+            # (np.array([[0.4, 0.6], [0.0, 0.0]]), -0.2),
         ],
     )
-    def test_acc(self, prev, result):
-        ep = ExtendedPrev(prev.flatten(), prev.shape[0], extpol=ExtensionPolicy())
-        assert error.acc(prev) == result
-        assert error.acc(ep) == result
-
-    @pytest.mark.parametrize(
-        "true_prev,estim_prev,nbcl,extpol,result",
-        [
-            (
-                [
-                    np.array([0.2, 0.4, 0.1, 0.3]),
-                    np.array([0.1, 0.5, 0.1, 0.3]),
-                ],
-                [
-                    np.array([0.3, 0.4, 0.2, 0.1]),
-                    np.array([0.5, 0.3, 0.1, 0.1]),
-                ],
-                2,
-                ExtensionPolicy(),
-                np.array([0.1, 0.2]),
-            ),
-            (
-                [
-                    np.array([0.2, 0.4, 0.4]),
-                    np.array([0.1, 0.5, 0.4]),
-                ],
-                [
-                    np.array([0.3, 0.4, 0.3]),
-                    np.array([0.5, 0.3, 0.2]),
-                ],
-                2,
-                ExtensionPolicy(collapse_false=True),
-                np.array([0.1, 0.2]),
-            ),
-            (
-                [
-                    np.array([0.02, 0.04, 0.16, 0.38, 0.1, 0.05, 0.15, 0.08, 0.02]),
-                    np.array([0.04, 0.02, 0.14, 0.40, 0.1, 0.03, 0.17, 0.07, 0.03]),
-                ],
-                [
-                    np.array([0.02, 0.04, 0.16, 0.48, 0.0, 0.05, 0.15, 0.08, 0.02]),
-                    np.array([0.14, 0.02, 0.04, 0.30, 0.2, 0.03, 0.17, 0.07, 0.03]),
-                ],
-                3,
-                ExtensionPolicy(),
-                np.array([0.1, 0.2]),
-            ),
-            (
-                [
-                    np.array([0.2, 0.4, 0.2, 0.2]),
-                    np.array([0.1, 0.3, 0.2, 0.4]),
-                ],
-                [
-                    np.array([0.3, 0.3, 0.1, 0.3]),
-                    np.array([0.5, 0.2, 0.1, 0.2]),
-                ],
-                3,
-                ExtensionPolicy(collapse_false=True),
-                np.array([0.1, 0.2]),
-            ),
-        ],
-    )
-    def test_accd(self, true_prev, estim_prev, nbcl, extpol, result):
-        true_prev = [ExtendedPrev(tp, nbcl, extpol=extpol) for tp in true_prev]
-        estim_prev = [ExtendedPrev(ep, nbcl, extpol=extpol) for ep in estim_prev]
-        _err = error.accd(true_prev, estim_prev)
-        assert (np.abs(_err - result) < 1e-15).all()
+    def test_k_macro(self, ct, result):
+        n = ct.shape[0]
+        print(f"{n=}")
+        # for i in range(n):
+        #     tp = ct[i, i]
+        #     fp = np.sum(ct[:, i]) - tp
+        #     fn = np.sum(ct[i, :]) - tp
+        #     tn = 1.0 - tp - fp - fn
+        #     tpr = tp / (tp + fn)
+        #     tnr = tn / (tn + fp)
+        #     print(f"case {i}")
+        #     print(f"{tp=}, {fp=}, {fn=}, {tn=}")
+        #     print(f"{tpr=}, {tnr=}")
+        #     print(f"k{i}={tpr + tnr - 1}")
+        #     print()
+        res = K_macro(ct)
+        print(res)
+        assert np.isclose(res, result)
