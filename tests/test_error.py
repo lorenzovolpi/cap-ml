5b8c0ac8b4f2425d3064b8b8c33de90d436ffcbe
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2025-12-19 12:28:13 +0100

    completed macro test for k

--- tests/test_error.py
+++ tests/test_error.py
@@ -1,12 +1,14 @@
+import itertools as IT
+
 import numpy as np
 import pytest
 from sklearn.metrics import balanced_accuracy_score
 
-from cap.error import K_bin, K_macro
+from cap.error import k_bin, k_macro
 
 
 def simulate_ct(ct: np.ndarray):
-    n_samples = 100000
+    n_samples = 500000
     n = ct.shape[0]
     couples = [(i, j) for i in range(n) for j in range(n)]
     counts = np.around(ct.flatten() * n_samples, decimals=0).astype("int")
@@ -14,19 +16,21 @@ def simulate_ct(ct: np.ndarray):
     y_couples = np.repeat(couples, counts, axis=0)
     y, y_hat = y_couples[:, 0], y_couples[:, 1]
 
-    # y_count = np.around(ct.sum(axis=1) * n_samples, decimals=0).astype("int")
-    # y_count[-1] += n_samples - y_count.sum()  # adjust for rounding errors
-    # y_hat_count = np.around(ct.sum(axis=0) * n_samples, decimals=0).astype("int")
-    # y_hat_count[-1] += n_samples - y_hat_count.sum()  # adjust for rounding errors
-    #
-    # y = np.repeat(np.arange(n), y_count)
-    # y_hat = np.repeat(np.arange(n), y_hat_count)
-
     return y, y_hat
 
 
+def macro_bacc_score(y, y_hat, n):
+    baccs = 0.0
+    for i in range(n):
+        yi = np.where(y == i, 1, 0)
+        y_hati = np.where(y_hat == i, 1, 0)
+        baccs += balanced_accuracy_score(yi, y_hati)
+
+    return baccs / n
+
+
 def k_bin_params():
-    repeats, n = 20, 2
+    repeats, n = 25, 2
     eps = 1e-3
     cts = []
     for _ in range(repeats):
@@ -34,18 +38,20 @@ def k_bin_params():
         ct += eps
         ct /= ct.sum()
         cts.append(ct)
+
     return cts
 
 
 def k_macro_params():
-    repeats, n = 20, 3
+    repeats, ns = 25, [3, 7, 11, 27]
     eps = 1e-3
     cts = []
-    for _ in range(repeats):
+    for n, _ in IT.product(ns, range(repeats)):
         ct = np.random.random(n**2).reshape(n, n)
         ct += eps
         ct /= ct.sum()
         cts.append(ct)
+
     return cts
 
 
@@ -60,41 +66,23 @@ def k_bin_fix(request):
 @pytest.fixture(params=k_macro_params())
 def k_macro_fix(request):
     ct = request.param
+    n = ct.shape[0]
     y, y_hat = simulate_ct(ct)
-    bacc = balanced_accuracy_score(y, y_hat) * 2 - 1
-    # bacc = balanced_accuracy_score(y, y_hat)
+    bacc = macro_bacc_score(y, y_hat, n) * 2 - 1
     yield ct, bacc
 
 
 class TestError:
     def test_k_bin(self, k_bin_fix):
         ct, result = k_bin_fix
-        assert np.isclose(K_bin(ct), result)
+        kres = k_bin(ct)
+        assert np.isclose(k_bin(ct), result, atol=1e-4)
+        assert kres >= -1.0 and kres <= 1.0
+        assert result >= -1.0 and result <= 1.0
 
-    # @pytest.mark.parametrize(
-    #     "ct,result",
-    #     [
-    #         (np.array([[0.2, 0.07, 0.25], [0.08, 0.1, 0.03], [0.07, 0.15, 0.05]]), 0.02381612),
-    #         # (np.array([[0.0, 0.0], [0.4, 0.6]]), 0.2),
-    #         # (np.array([[0.4, 0.6], [0.0, 0.0]]), -0.2),
-    #     ],
-    # )
     def test_k_macro(self, k_macro_fix):
         ct, result = k_macro_fix
-        n = ct.shape[0]
-        print(f"{n=}")
-        # for i in range(n):
-        #     tp = ct[i, i]
-        #     fp = np.sum(ct[:, i]) - tp
-        #     fn = np.sum(ct[i, :]) - tp
-        #     tn = 1.0 - tp - fp - fn
-        #     tpr = tp / (tp + fn)
-        #     tnr = tn / (tn + fp)
-        #     print(f"case {i}")
-        #     print(f"{tp=}, {fp=}, {fn=}, {tn=}")
-        #     print(f"{tpr=}, {tnr=}")
-        #     print(f"k{i}={tpr + tnr - 1}")
-        #     print()
-        res = K_macro(ct)
-        print(res)
-        assert np.isclose(res, result)
+        kres = k_macro(ct)
+        assert np.isclose(k_macro(ct), result, atol=1e-4)
+        assert kres >= -1.0 and kres <= 1.0
+        assert result >= -1.0 and result <= 1.0
