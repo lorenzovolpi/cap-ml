172c337cd707de3e8990ee6afa09b1ee70564e60
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2025-12-19 12:45:00 +0100

    added test for k measure

--- tests/test_error.py
+++ tests/test_error.py
@@ -1,95 +1,88 @@
+import itertools as IT
+
 import numpy as np
 import pytest
+from sklearn.metrics import balanced_accuracy_score
 
-from quacc import error
-from quacc.legacy.data import ExtendedPrev, ExtensionPolicy
+from cap.error import k_bin, k_macro
 
 
-@pytest.mark.err
-class TestError:
-    @pytest.mark.parametrize(
-        "prev,result",
-        [
-            (np.array([[1, 4], [4, 4]]), 0.5),
-            (np.array([[6, 2, 4], [2, 4, 2], [4, 2, 6]]), 0.5),
-        ],
-    )
-    def test_f1(self, prev, result):
-        ep = ExtendedPrev(prev.flatten(), prev.shape[0], extpol=ExtensionPolicy())
-        assert error.f1(prev) == result
-        assert error.f1(ep) == result
+def simulate_ct(ct: np.ndarray):
+    n_samples = 500000
+    n = ct.shape[0]
+    couples = [(i, j) for i in range(n) for j in range(n)]
+    counts = np.around(ct.flatten() * n_samples, decimals=0).astype("int")
+    counts[-1] += n_samples - counts.sum()  # adjust for rounding errors
+    y_couples = np.repeat(couples, counts, axis=0)
+    y, y_hat = y_couples[:, 0], y_couples[:, 1]
 
-    @pytest.mark.parametrize(
-        "prev,result",
-        [
-            (np.array([[4, 4], [4, 4]]), 0.5),
-            (np.array([[2, 4, 2], [2, 2, 4], [4, 2, 2]]), 0.25),
-        ],
-    )
-    def test_acc(self, prev, result):
-        ep = ExtendedPrev(prev.flatten(), prev.shape[0], extpol=ExtensionPolicy())
-        assert error.acc(prev) == result
-        assert error.acc(ep) == result
+    return y, y_hat
 
-    @pytest.mark.parametrize(
-        "true_prev,estim_prev,nbcl,extpol,result",
-        [
-            (
-                [
-                    np.array([0.2, 0.4, 0.1, 0.3]),
-                    np.array([0.1, 0.5, 0.1, 0.3]),
-                ],
-                [
-                    np.array([0.3, 0.4, 0.2, 0.1]),
-                    np.array([0.5, 0.3, 0.1, 0.1]),
-                ],
-                2,
-                ExtensionPolicy(),
-                np.array([0.1, 0.2]),
-            ),
-            (
-                [
-                    np.array([0.2, 0.4, 0.4]),
-                    np.array([0.1, 0.5, 0.4]),
-                ],
-                [
-                    np.array([0.3, 0.4, 0.3]),
-                    np.array([0.5, 0.3, 0.2]),
-                ],
-                2,
-                ExtensionPolicy(collapse_false=True),
-                np.array([0.1, 0.2]),
-            ),
-            (
-                [
-                    np.array([0.02, 0.04, 0.16, 0.38, 0.1, 0.05, 0.15, 0.08, 0.02]),
-                    np.array([0.04, 0.02, 0.14, 0.40, 0.1, 0.03, 0.17, 0.07, 0.03]),
-                ],
-                [
-                    np.array([0.02, 0.04, 0.16, 0.48, 0.0, 0.05, 0.15, 0.08, 0.02]),
-                    np.array([0.14, 0.02, 0.04, 0.30, 0.2, 0.03, 0.17, 0.07, 0.03]),
-                ],
-                3,
-                ExtensionPolicy(),
-                np.array([0.1, 0.2]),
-            ),
-            (
-                [
-                    np.array([0.2, 0.4, 0.2, 0.2]),
-                    np.array([0.1, 0.3, 0.2, 0.4]),
-                ],
-                [
-                    np.array([0.3, 0.3, 0.1, 0.3]),
-                    np.array([0.5, 0.2, 0.1, 0.2]),
-                ],
-                3,
-                ExtensionPolicy(collapse_false=True),
-                np.array([0.1, 0.2]),
-            ),
-        ],
-    )
-    def test_accd(self, true_prev, estim_prev, nbcl, extpol, result):
-        true_prev = [ExtendedPrev(tp, nbcl, extpol=extpol) for tp in true_prev]
-        estim_prev = [ExtendedPrev(ep, nbcl, extpol=extpol) for ep in estim_prev]
-        _err = error.accd(true_prev, estim_prev)
-        assert (np.abs(_err - result) < 1e-15).all()
+
+def macro_bacc_score(y, y_hat, n):
+    baccs = 0.0
+    for i in range(n):
+        yi = np.where(y == i, 1, 0)
+        y_hati = np.where(y_hat == i, 1, 0)
+        baccs += balanced_accuracy_score(yi, y_hati)
+
+    return baccs / n
+
+
+def k_bin_params():
+    repeats, n = 25, 2
+    eps = 1e-3
+    cts = []
+    for _ in range(repeats):
+        ct = np.random.random(n**2).reshape(n, n)
+        ct += eps
+        ct /= ct.sum()
+        cts.append(ct)
+
+    return cts
+
+
+def k_macro_params():
+    repeats, ns = 25, [3, 7, 11, 27]
+    eps = 1e-3
+    cts = []
+    for n, _ in IT.product(ns, range(repeats)):
+        ct = np.random.random(n**2).reshape(n, n)
+        ct += eps
+        ct /= ct.sum()
+        cts.append(ct)
+
+    return cts
+
+
+@pytest.fixture(params=k_bin_params())
+def k_bin_fix(request):
+    ct = request.param
+    y, y_hat = simulate_ct(ct)
+    bacc = balanced_accuracy_score(y, y_hat) * 2 - 1
+    yield ct, bacc
+
+
+@pytest.fixture(params=k_macro_params())
+def k_macro_fix(request):
+    ct = request.param
+    n = ct.shape[0]
+    y, y_hat = simulate_ct(ct)
+    bacc = macro_bacc_score(y, y_hat, n) * 2 - 1
+    yield ct, bacc
+
+
+class TestMeasureK:
+    def test_k_bin(self, k_bin_fix):
+        ct, result = k_bin_fix
+        kres = k_bin(ct)
+        assert np.isclose(k_bin(ct), result, atol=1e-4)
+        assert kres >= -1.0 and kres <= 1.0
+        assert result >= -1.0 and result <= 1.0
+
+    def test_k_macro(self, k_macro_fix):
+        ct, result = k_macro_fix
+        kres = k_macro(ct)
+        assert np.isclose(k_macro(ct), result, atol=1e-4)
+        assert kres >= -1.0 and kres <= 1.0
+        assert result >= -1.0 and result <= 1.0
