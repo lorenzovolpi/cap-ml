d4f7ea4990ea4e723500152e9f98f5396685b73a
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2026-02-21 11:32:54 +0100

    quickfix for probabilities summing to 1

--- src/cap/models/direct.py
+++ src/cap/models/direct.py
@@ -549,7 +549,7 @@ class RQBS(CAPDirect):
             with qp.util.temp_seed(self.random_state):
                 for _ in range(self.n_vsamples):
                     sample_i = resample(classif_predictions, n_samples=n_samples)
-                    prev_i = self.q.aggregate(sample_i)
+                    prev_i = utils.smooth(self.q.aggregate(sample_i))
                     prevs.append(prev_i)
             val_samples_idx = [self.val_post.sampling_index(self.sample_size, *q_hat) for q_hat in prevs]
         else:
