52befb8155d32b9cff210d685778969d5c73d1f2
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2026-04-09 14:04:09 +0200

    removed functions relative to local path

--- src/cap_exp/util.py
+++ src/cap_exp/util.py
@@ -97,33 +97,6 @@ def split_validation(V: LabelledCollection, ratio=0.6, repeats=100, sample_size=
     return v_train, val_prot
 
 
-def local_path(domain, dataset_name, cls_name, method_name, acc_name, experiment=None, format="parquet"):
-    base_dir = env.root_dir if experiment is None else os.path.join(env.root_dir, experiment)
-    parent_dir = os.path.join(base_dir, domain, acc_name, dataset_name, method_name)
-    os.makedirs(parent_dir, exist_ok=True)
-    return os.path.join(parent_dir, f"{cls_name}.{format}")
-
-
-# def all_results_exist(dataset_name: str, h_name: str, method_names: list[str], acc_names: list[str], experiment: str):
-#     exists = True
-#     for acc_name, method_name in IT.product(acc_names, method_names):
-#         if not os.path.exists(local_path(dataset_name, h_name, method_name, acc_name, experiment=experiment)):
-#             exists = False
-#             break
-#     return exists
-
-
-def all_results_exist(domain, dataset_name, cls_name, method_names, acc_names, experiment=None):
-    all_exist = True
-    for method, acc in IT.product(method_names, acc_names):
-        path = local_path(domain, dataset_name, cls_name, method, acc, experiment=experiment)
-        all_exist = os.path.exists(path)
-        if not all_exist:
-            break
-
-    return all_exist
-
-
 def decorate_dataset(dataset: str):
     return r"\textsf{" + dataset + r"}"
 
