cfc2ba306f441fde65ee9a2b6a4863fe0cb13ff9
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2026-05-29 00:53:32 +0200

    fixed method imports

--- src/cap/models/__init__.py
+++ src/cap/models/__init__.py
@@ -1,3 +1,3 @@
-from cap.models.confidence import ConfidenceInterval
-from cap.models.cont_table import CBPE, LEAP, O_LEAP, S_LEAP, NaiveCAP, QuAcc1xN2, QuAcc1xNN, QuAcc1xNp1, QuAccNxN
+from cap.models.confidence import CBPE, RQBS, ACC_BayesCAP, ConfidenceInterval, PrediQuant
+from cap.models.cont_table import LEAP, O_LEAP, S_LEAP, NaiveCAP, QuAcc1xN2, QuAcc1xNN, QuAcc1xNp1, QuAccNxN
 from cap.models.direct import ATC, COT, COTT, Q_COT, DispersionScore, DoC, NuclearNorm
